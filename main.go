75bd14da0d4d6fe7e11004148bd49c8f03168499
Asim Aslam <asim@aslam.me>
2021-12-16 20:50:00 +0000

    .

--- main.go
+++ main.go
@@ -6,7 +6,46 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`hello world`))
+		message := "Hello world!"
+
+		ct := r.Header.Get("Content-Type")
+
+		if ct == "application/json" {
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(500)
+				return
+			}
+
+			var req request
+
+			if err := json.Unmarshal(b, &req); err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			if len(req.Name) > 0 {
+				message = "Hello " + req.Name + "!"
+			}
+
+			if err := json.NewEncoder(w).Encode(response{Message: message}); err != nil {
+				w.WriteHeader(500)
+				return
+			}
+
+			return
+		}
+
+		r.ParseForm()
+		name := r.Form.Get("name")
+
+		if len(name) > 0 {
+			message = "Hello " + name + "!"
+		}
+
+		w.Write([]byte(message))
 	})
+
 	http.ListenAndServe(":8080", nil)
 }
